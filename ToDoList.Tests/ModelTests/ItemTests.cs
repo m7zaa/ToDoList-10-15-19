9f998535c4dc3c8b698acc4ca37e3060b45c2a38
Brendon Tse and Loewy Malkovich <loewy@>
2019-10-14 16:24:36 -0700

    add find and save methods/tests

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -47,21 +47,40 @@
         //     Assert.AreEqual(1, result);
         // }
 
-        //     [TestMethod]
-        // public void Find_ReturnsCorrectItem_Item()
-        // {
-        //     //Arrange
-        //     string description01 = "Walk the dog";
-        //     string description02 = "Wash the dishes";
-        //     Item newItem1 = new Item(description01);
-        //     Item newItem2 = new Item(description02);
+            [TestMethod]
+        public void Find_ReturnsCorrectItem_Item()
+        {
+            //Arrange
+            string description01 = "Walk the dog";
+            string description02 = "Wash the dishes";
+            Item newItem1 = new Item(description01);
+            Item newItem2 = new Item(description02);
 
-        //     //Act
-        //     Item result = Item.Find(2);
+            //Act
+            Item result = Item.Find(2);
 
-        //     //Assert
-        //     Assert.AreEqual(newItem2, result);
-        // }
+            //Assert
+            Assert.AreEqual(newItem2, result);
+        }
+
+            [TestMethod]
+        public void GetAll_ReturnsItems_ItemList()
+        {
+            //Arrange
+            string description01 = "Walk the dog";
+            string description02 = "Wash the dishes";
+            Item newItem1 = new Item(description01);
+            newItem1.Save(); // New code
+            Item newItem2 = new Item(description02);
+            newItem2.Save(); // New code
+            List<Item> newList = new List<Item> { newItem1, newItem2 };
+
+            //Act
+            List<Item> result = Item.GetAll();
+
+            //Assert
+            CollectionAssert.AreEqual(newList, result);
+        }
 
             [TestMethod]
         public void Equals_ReturnsTrueIfDescriptionsAreTheSame_Item()
@@ -72,6 +91,21 @@
 
         // Assert
         Assert.AreEqual(firstItem, secondItem);
+        }
+
+            [TestMethod]
+        public void Save_SavesToDatabase_ItemList()
+        {
+            //Arrange
+            Item testItem = new Item("Mow the lawn");
+
+            //Act
+            testItem.Save();
+            List<Item> result = Item.GetAll();
+            List<Item> testList = new List<Item>{testItem};
+
+            //Assert
+            CollectionAssert.AreEqual(testList, result);
         }
 
     }
--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -6,7 +6,7 @@ namespace ToDoList.Models
   public class Item
   {
     public string Description { get; set; }
-    public int Id { get; }
+    public int Id { get; set; }
 
     public Item(string description)
     {
@@ -58,11 +58,41 @@ namespace ToDoList.Models
      }
     }
 
-    public static Item Find(int searchId)
-    {
-      Item placeholderItem = new Item("placeholder item");
-      return placeholderItem;
-    }
+    public static Item Find(int id)
+{
+  // We open a connection.
+  MySqlConnection conn = DB.Connection();
+  conn.Open();
+
+  // We create MySqlCommand object and add a query to its CommandText property. We always need to do this to make a SQL query.
+  var cmd = conn.CreateCommand() as MySqlCommand;
+  cmd.CommandText = @"SELECT * FROM `items` WHERE id = @thisId;";
+
+  // We have to use parameter placeholders (@thisId) and a `MySqlParameter` object to prevent SQL injection attacks. This is only necessary when we are passing parameters into a query. We also did this with our Save() method.
+  MySqlParameter thisId = new MySqlParameter();
+  thisId.ParameterName = "@thisId";
+  thisId.Value = id;
+  cmd.Parameters.Add(thisId);
+
+  // We use the ExecuteReader() method because our query will be returning results and we need this method to read these results. This is in contrast to the ExecuteNonQuery() method, which we use for SQL commands that don't return results like our Save() method.
+  var rdr = cmd.ExecuteReader() as MySqlDataReader;
+  int itemId = 0;
+  string itemDescription = "";
+  while (rdr.Read())
+  {
+     itemId = rdr.GetInt32(0);
+     itemDescription = rdr.GetString(1);
+  }
+  Item foundItem= new Item(itemDescription, itemId);
+
+  // We close the connection.
+  conn.Close();
+  if (conn != null)
+  {
+    conn.Dispose();
+  }
+  return foundItem;
+}
 
     public override bool Equals(System.Object otherItem)
     {
@@ -73,10 +103,36 @@ namespace ToDoList.Models
       else
       {
         Item newItem = (Item) otherItem;
+        bool idEquality = (this.Id == newItem.Id);
         bool descriptionEquality = (this.Description == newItem.Description);
-        return descriptionEquality;
+        return (idEquality && descriptionEquality);
       }
     }
-    
+
+    public void Save()
+    {
+    MySqlConnection conn = DB.Connection();
+    conn.Open();
+    var cmd = conn.CreateCommand() as MySqlCommand;
+
+    // Begin new code
+
+    cmd.CommandText = @"INSERT INTO items (description) VALUES (@ItemDescription);";
+    MySqlParameter description = new MySqlParameter();
+    description.ParameterName = "@ItemDescription";
+    description.Value = this.Description;
+    cmd.Parameters.Add(description);    
+    cmd.ExecuteNonQuery();
+    Id = (int) cmd.LastInsertedId;
+
+    // End new code
+
+    conn.Close();
+    if (conn != null)
+    {
+      conn.Dispose();
+    }
+      }
+
   }
 }
