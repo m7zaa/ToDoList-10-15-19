36136d227905f786593ec3c3c107f50a1b04e02a
loewy malkovich and micah zaayer <micah@>
2019-10-15 16:02:18 -0700

    change category model and controller, add html pages for categories

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -1,57 +1,55 @@
-// using System.Collections.Generic;
-// using System;
-// using Microsoft.AspNetCore.Mvc;
-// using ToDoList.Models;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System.Linq;
 
-// namespace ToDoList.Controllers
-// {
-//   public class CategoriesController : Controller
-//   {
+namespace ToDoList.Controllers
+{
+  public class CategoriesController : Controller
+  {
+    private readonly ToDoListContext _db;
 
-//     [HttpGet("/categories")]
-//     public ActionResult Index()
-//     {
-//       List<Category> allCategories = Category.GetAll();
-//       return View(allCategories);
-//     }
+    public CategoriesController(ToDoListContext db)
+    {
+      _db = db;
+    }
 
-//     [HttpGet("/categories/new")]
-//     public ActionResult New()
-//     {
-//       return View();
-//     }
+    public ActionResult Index()
+    {
+      List<Category> model = _db.Categories.ToList();
+      return View(model);
+    }
+    public ActionResult Create()
+    {
+        return View();
+    }
 
-//     [HttpPost("/categories")]
-//     public ActionResult Create(string categoryName)
-//     {
-//       Category newCategory = new Category(categoryName);
-//       return RedirectToAction("Index");
-//     }
+    [HttpPost]
+    public ActionResult Create(Category category)
+    {
+        _db.Categories.Add(category);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+    // public ActionResult Details(int id)
+    // {
+    //     Category thisCategory = _db.Categories.FirstOrDefault(categories => categories.CategoryId == id);
+    //     return View(thisCategory);
+    // }
 
-//     [HttpGet("/categories/{id}")]
-//     public ActionResult Show(int id)
-//     {
-//       Dictionary<string, object> model = new Dictionary<string, object>();
-//       Category selectedCategory = Category.Find(id);
-//       List<Item> categoryItems = selectedCategory.Items;
-//       model.Add("category", selectedCategory);
-//       model.Add("items", categoryItems);
-//       return View(model);
-//     }
+    public ActionResult Delete(int id)
+    {
+        var thisCategory = _db.Categories.FirstOrDefault(categories => categories.CategoryId == id);
+        return View(thisCategory);
+    }
 
-//     [HttpPost("/categories/{categoryId}/items")]
-//     public ActionResult Create(int categoryId, string itemDescription)
-//     {
-//       Dictionary<string, object> model = new Dictionary<string, object>();
-//       Category foundCategory = Category.Find(categoryId);
-//       Item newItem = new Item(itemDescription);
-//       newItem.Save();    // New code
-//       foundCategory.AddItem(newItem);
-//       List<Item> categoryItems = foundCategory.Items;
-//       model.Add("items", categoryItems);
-//       model.Add("category", foundCategory);
-//       return View("Show", model);
-//     }
-
-//   }
-// }
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+        var thisCategory = _db.Categories.FirstOrDefault(categories => categories.CategoryId == id);
+        _db.Categories.Remove(thisCategory);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+  }
+}
--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -1,7 +1,10 @@
+  
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ToDoList.Controllers
 {
@@ -16,11 +19,13 @@ namespace ToDoList.Controllers
 
     public ActionResult Index()
     {
-      List<Item> model = _db.Items.ToList();
-      return View(model);
+        List<Item> model = _db.Items.Include(items => items.Category).ToList();
+        return View(model);
     }
+
     public ActionResult Create()
     {
+        ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
         return View();
     }
 
@@ -31,10 +36,33 @@ namespace ToDoList.Controllers
         _db.SaveChanges();
         return RedirectToAction("Index");
     }
+
+    public ActionResult Edit(int id)
+    {
+        var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+        ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+        return View(thisItem);
+    }
+
     public ActionResult Details(int id)
-{
-    Item thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
-    return View(thisItem);
-}
+    {
+        Item thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+        return View(thisItem);
+    }
+
+    public ActionResult Delete(int id)
+    {
+        var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+        return View(thisItem);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+        var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+        _db.Items.Remove(thisItem);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
   }
 }
