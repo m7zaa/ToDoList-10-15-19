220dfbdd2abe911aad8c42dc0eb6855270150260
Student <example@example.com>
2019-10-21 11:10:58 -0700

    add many-to-many relationship and CategoryItem class.

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -2,54 +2,74 @@ using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace ToDoList.Controllers
 {
-  public class CategoriesController : Controller
-  {
-    private readonly ToDoListContext _db;
+    public class CategoriesController : Controller
+    {
+        private readonly ToDoListContext _db;
 
-    public CategoriesController(ToDoListContext db)
-    {
-      _db = db;
-    }
+        public CategoriesController(ToDoListContext db)
+        {
+          _db = db;
+        }
 
-    public ActionResult Index()
-    {
-      List<Category> model = _db.Categories.ToList();
-      return View(model);
-    }
-    public ActionResult Create()
-    {
-        return View();
-    }
+        public ActionResult Index()
+        {
+          List<Category> model = _db.Categories.ToList();
+          return View(model);
+        }
 
-    [HttpPost]
-    public ActionResult Create(Category category)
-    {
-        _db.Categories.Add(category);
-        _db.SaveChanges();
-        return RedirectToAction("Index");
-    }
-    public ActionResult Details(int id)
-    {
-        Category thisCategory = _db.Categories.FirstOrDefault(categories => categories.CategoryId == id);
-        return View(thisCategory);
-    }
+        public ActionResult Create()
+        {
+          return View();
+        }
 
-    public ActionResult Delete(int id)
-    {
-        var thisCategory = _db.Categories.FirstOrDefault(categories => categories.CategoryId == id);
-        return View(thisCategory);
-    }
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+          _db.Categories.Add(category);
+          _db.SaveChanges();
+          return RedirectToAction("Index");
+        }
 
-    [HttpPost, ActionName("Delete")]
-    public ActionResult DeleteConfirmed(int id)
-    {
-        var thisCategory = _db.Categories.FirstOrDefault(categories => categories.CategoryId == id);
-        _db.Categories.Remove(thisCategory);
-        _db.SaveChanges();
-        return RedirectToAction("Index");
+        public ActionResult Details(int id)
+        {
+            var thisCategory = _db.Categories
+                .Include(category => category.Items)
+                .ThenInclude(join => join.Item)
+                .FirstOrDefault(category => category.CategoryId == id);
+            return View(thisCategory);
+        }
+
+        public ActionResult Edit(int id)
+        {
+          var thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
+          return View(thisCategory);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Category category)
+        {
+          _db.Entry(category).State = EntityState.Modified;
+          _db.SaveChanges();
+          return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+          var thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
+          return View(thisCategory);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+          var thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
+          _db.Categories.Remove(thisCategory);
+          _db.SaveChanges();
+          return RedirectToAction("Index");
+        }
     }
-  }
 }
